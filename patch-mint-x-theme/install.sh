317c394cd1b7163c5523cb676d61d6da2a9df25f
Andrew Thornton <art27@cantab.net>
2014-09-02 17:34:47 +0100

    Add check to ensure dconf-tools are installed

--- patch-mint-x-theme/install.sh
+++ patch-mint-x-theme/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+command -v dconf >/dev/null 2>&1 || { echo >&2 "I require dconf, part of dconf-tools, but this doesn't exist. Please install dconf-tools: sudo apt-get install dconf-tools"; exit 1; }
 cp -r /usr/share/themes/Mint-X Mint-X-Max
 cd Mint-X-Max
 patch -p1 < ../theme.patch
--- patch-mint-x-theme/uninstall.sh
+++ patch-mint-x-theme/uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+command -v dconf >/dev/null 2>&1 || { echo >&2 "I require dconf, part of dconf-tools, but this doesn't exist. Please install dconf-tools: sudo apt-get install dconf-tools"; exit 1; }
 export KEY="/org/cinnamon/desktop/wm/preferences/theme"
 dconf write "$KEY" "'Mint-X'"
 rm -r ~/.themes/Mint-X-Max
