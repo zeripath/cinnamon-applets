ab38f3aaf2304bf4b2a5a8d0dc26c5bedaa31e91
Andrew Thornton <art27@cantab.net>
2017-06-28 12:16:28 +0100

    [window-list] Ensure that settings-schema.json is copied

--- window-list-applet/install.sh
+++ window-list-applet/install.sh
@@ -4,6 +4,6 @@ export KEY="/org/cinnamon/enabled-applets"
 VALUE="$(dconf read $KEY | sed -e 's+window-list@cinnamon.org+window-list@zeripath.sdf-eu.org+')"
 DIR="$HOME/.local/share/cinnamon/applets/window-list@zeripath.sdf-eu.org"
 mkdir -p "$DIR"
-cp applet.js metadata.json $DIR
+cp applet.js metadata.json settings-schema.json $DIR
 dconf write "$KEY" "$VALUE"
 
